61f06b042872171be57c2202c42d5327a071b564
Alarapon Silas <alarapontop04@gmail.com>
2022-10-13 11:07:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  *  main - print the string in the put function
  *
- * * this program prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  *
  * Return: 0
  * 
