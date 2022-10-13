7a65422739e04fc7a5ea0db696fd473eb9a13534
Alarapon Silas <alarapontop04@gmail.com>
2022-10-13 11:37:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the string 'with proper grammer,
+ * but the outcome is a piece of art,
+ * Return: 0
+ */
+int main(void)
+{
+	printf("WIth proper grammar, but the outcomeis a piece of art,\n");
+	return(0);
+}
