7afe85449559e9525827e64fde85e34724f6cabf
Alarapon Silas <alarapontop04@gmail.com>
2022-10-14 12:10:54 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	int n;
-	int p;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
@@ -24,3 +24,4 @@ int main(void)
 		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
+}
