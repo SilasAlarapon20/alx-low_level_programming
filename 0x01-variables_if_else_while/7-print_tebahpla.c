0b0a04875a55c770267ff6e134d4ed7ca12d5836
Alarapon Silas <alarapontop04@gmail.com>
2022-10-14 14:44:30 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,14 +4,16 @@
  * main - Prints the lowercase alphabet in reverse.
  * Return: Always 0.
  */
-
 int main(void)
 {
 	char c;
+
 	for (c = 'z'; c >= 'a'; c--)
 	{
 		putchar(c);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
