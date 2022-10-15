dd7bd31ab3def36c75cc6be915bf9bb310dcaf70
Alarapon Silas <alarapontop04@gmail.com>
2022-10-14 19:54:28 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,23 +7,26 @@
  */
 int main(void)
 {
-	int num1, num2;
+	int i, j;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (j = 0; j <= 99; j++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			
-			if (num1 == 98 && num2 == 99}
-				continue;
-			
-			putchar(',');
-			putchar(' ');
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 
