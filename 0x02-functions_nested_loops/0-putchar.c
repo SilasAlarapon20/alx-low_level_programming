a0f0b6912677d297b4a0d34ebe2fe50b1cb80cfb
Alarapon Silas <alarapontop04@gmail.com>
2022-10-18 07:27:28 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,9 @@ int main(void)
 	char ch[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r',};
 	unsigned int c;
 
-	for (p = 0; p < sizeof(ch); p++)
+	for (c = 0; c < sizeof(ch); c++)
 	{
 		_putchar(ch[c]);
 	}
 	_putchar('\n');
-	return (0)
+	return (0);
