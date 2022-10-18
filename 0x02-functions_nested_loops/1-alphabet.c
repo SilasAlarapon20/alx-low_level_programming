c60030a58a28cbc20b0ed96eac98a6210e9ea009
Alarapon Silas <alarapontop04@gmail.com>
2022-10-18 08:18:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * A function that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
