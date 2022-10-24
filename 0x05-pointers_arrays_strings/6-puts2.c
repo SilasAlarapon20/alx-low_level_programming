f5581fb5a2d6395f9beb882df61d33e9ff4606a4
Alarapon Silas <alarapontop04@gmail.com>
2022-10-24 11:32:09 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * a function that prints every other character of a string,
+ * puts2 - a function that prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: declaration of str and paramters
  * Return: 0
