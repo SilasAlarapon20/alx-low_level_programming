fbe89e36736779756f43dad3caded0879dfcc881
Alarapon Silas <alarapontop04@gmail.com>
2022-10-27 04:48:34 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,9 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+int main(void);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size;
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif
