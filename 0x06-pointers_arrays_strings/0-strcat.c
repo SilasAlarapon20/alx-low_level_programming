0fc40a17922d7ac3acd4a685946c2614601cbc13
Alarapon Silas <alarapontop04@gmail.com>
2022-10-27 04:35:20 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a, b;
+	int a = 0, b = 0;
 
-	for (a = 0; dest[a] != '\0'; a++)
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
 	{
+		dest[a] = src[b];
+		b++;
+		a++;
 	}
-	for (b = 0; (dest[a + b] = *src++) != '\0'; b++)
-	{
-	}
-	return (dest);
+	return (dest)
 }
